3136b1a2d36791598287a574d5cd6853227fbc83
Jed Kutai <jedkutai@gmail.com>
2024-06-01 02:17:57 -0400

    Initial Commit

---
+++ Simplified/SimplifiedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplifiedApp.swift
+//  Simplified
+//
+//  Created by Jed Kutai on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SimplifiedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
